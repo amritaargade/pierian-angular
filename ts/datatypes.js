c67aeff6f3bfb260757c3935d9644a949049dbdc
amritaargade <amrita7495@gmail.com>
2019-08-05 14:44:29 +0530

    type:datatype

---
+++ ts/datatypes.js
@@ -0,0 +1,7 @@
+var num = 10;
+var str = 'hii';
+var obj = {
+    age: 'sani',
+    name: 'amrita'
+};
+var shape = true;
---
+++ ts/datatypes.ts
@@ -0,0 +1,7 @@
+let num : number = 10
+let str : String ='hii'
+let obj : { [ key: string] : any} = {
+    age :'sani',
+    name :'amrita'
+}
+let shape : number | String |boolean = true
